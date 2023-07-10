e79209701000b6ced0cad3e64a9410aa6e2af4eb
mzamarri <miguelazamarripar@gmail.com>
2023-07-10 02:29:50 -0400

    Made final changes using Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,7 +20,7 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/async-get-all-books',function (req, res) {
 
   const get_books = new Promise((resolve, reject) => {
       resolve(res.send(JSON.stringify(books, null, 4)))
@@ -29,10 +29,10 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const ISBN = req.params.isbn;
-  let book =  books[ISBN];
+public_users.get('/async-get-by-isbn/:isbn',function (req, res) {
   const book_details = new Promise((resolve, reject) => {
+    const ISBN = req.params.isbn;
+    let book =  books[ISBN];
     resolve(res.send(JSON.stringify(book, null, 4)))
 })
   book_details.then(() => console.log("Task 11 completed successfully!"))
@@ -53,21 +53,32 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // ^ with Promises
-public_users.get('/async-filter-by-author/:author',function (req, res) {
-    const author = req.params.author;
-    let ISBN_list = Object.keys(books).map(isbn => parseInt(isbn));
-    let filtered_books = [];
+public_users.get('/async-filter-by-author/:author', function (req, res) {
     const get_books_by_author = new Promise((resolve, reject) => {
-        resolve()
-    })
-    for (let i=0; i < ISBN_list.length; i++) {
-        ISBN = ISBN_list[i];
-        if (author === books[ISBN]["author"]) {
-            filtered_books.push(books[ISBN])
+        const author = req.params.author;
+        let ISBN_list = Object.keys(books).map(isbn => parseInt(isbn));
+        let filtered_books = [];
+        for (let i=0; i < ISBN_list.length; i++) {
+            ISBN = ISBN_list[i];
+            if (author === books[ISBN]["author"]) {
+                filtered_books.push(books[ISBN])
+            }
         }
-    }
-    res.send(JSON.stringify(filtered_books, null, 4))
-  });
+        if (filtered_books) {
+            resolve(res.send(JSON.stringify(filtered_books, null, 4)))
+        } else {
+            reject(author)
+        }
+        
+    })
+
+    get_books_by_author.then(() => {
+        console.log("Task 12 complete!");
+        console.log("Books from that author.")
+    })
+
+    get_books_by_author.catch((author) => console.log(`No books by ${author}.`));
+});
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
@@ -81,6 +92,33 @@ public_users.get('/title/:title',function (req, res) {
         }
     }
     res.send(JSON.stringify(filtered_books, null, 4));
+});
+
+public_users.get('/async-filter-by-title/:title', function (req, res) {
+    const get_books_by_title = new Promise((resolve, reject) => {
+        const title = req.params.title;
+        let ISBN_list = Object.keys(books).map(isbn => parseInt(isbn));
+        let filtered_books = [];
+        for (let i=0; i < ISBN_list.length; i++) {
+            ISBN = ISBN_list[i];
+            if (title === books[ISBN]["title"]) {
+                filtered_books.push(books[ISBN])
+            }
+        }
+        if (filtered_books) {
+            resolve(res.send(JSON.stringify(filtered_books, null, 4)))
+        } else {
+            reject(title)
+        }
+        
+    })
+
+    get_books_by_title.then(() => {
+        console.log("Task 12 complete!");
+        console.log("Books from that author.")
+    })
+
+    get_books_by_title.catch((author) => console.log(`No books by ${title}.`));
 });
 
 //  Get book review
